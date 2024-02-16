4e5b4efac8c95ffc919268ced2c89eb7c3548846
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-02-16 20:43:06 +0100

    Update LoadMoreBtn.jsx

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,5 +1,7 @@
 import style from './LoadMoreBtn.module.css'
 
 export const LoadMoreBtn = ({onClick}) => {
-    return <button type="submit" onClick={onClick} className={style.button}>Load more images</button>
+    return <button type="submit"
+        onClick={onClick}
+        className={style.button}>Load more images</button>
 };
